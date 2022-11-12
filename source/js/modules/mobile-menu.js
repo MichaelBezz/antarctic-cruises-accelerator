5bfb6005edb29cf4f0c0024031423b5bb591af9e
Michael Bezzaponnyy <michael.bezzaponnyy@gmail.com>
2022-11-12 15:03:29 +0300

    Реализация мобильного меню

--- source/js/modules/mobile-menu.js
+++ source/js/modules/mobile-menu.js
@@ -1,23 +1,71 @@
 import {ScrollLock} from '../../js/utils/scroll-lock';
+import {createSmoothEffect} from '../utils/scroll-smooth';
+
+const DefaultPosition = {
+  LOGO_OPEN: 20,
+  LOGO_CLOSE: 0,
+  TOGGLE_OPEN: 28,
+  TOGGLE_CLOSE: 0,
+};
 
 const mobileMenu = document.querySelector('[data-mobile-menu]');
+const mobileMenuLogo = mobileMenu.querySelector('[data-mobile-menu-logo]');
 const mobileMenuToggle = mobileMenu.querySelector('[data-mobile-menu-toggle]');
+const mobileMenuNavigation = mobileMenu.querySelector('[data-mobile-menu-navigation]');
 
 const scrollLock = new ScrollLock();
 
 export const initializeMobileMenu = () => {
-  if (mobileMenu && mobileMenuToggle) {
-    mobileMenuToggle.addEventListener('click', () => {
-      if (mobileMenu.classList.contains('is-close')) {
-        mobileMenu.classList.remove('is-close');
-        mobileMenu.classList.add('is-open');
+  if (mobileMenu && mobileMenuLogo && mobileMenuToggle && mobileMenuNavigation) {
 
-        scrollLock.disableScrolling();
-      } else {
-        mobileMenu.classList.add('is-close');
-        mobileMenu.classList.remove('is-open');
+    const close = () => {
+      mobileMenu.classList.remove('is-open');
+      mobileMenu.classList.add('is-close');
 
-        scrollLock.enableScrolling();
+      mobileMenuLogo.style.top = `${DefaultPosition.LOGO_CLOSE}px`;
+      mobileMenuToggle.style.top = `${DefaultPosition.TOGGLE_CLOSE}px`;
+
+      scrollLock.enableScrolling();
+    };
+
+    const open = () => {
+      mobileMenu.classList.remove('is-close');
+      mobileMenu.classList.add('is-open');
+
+      mobileMenuNavigation.scrollTop = 0;
+      mobileMenuLogo.style.top = `${DefaultPosition.LOGO_OPEN}px`;
+      mobileMenuToggle.style.top = `${DefaultPosition.TOGGLE_OPEN}px`;
+
+      scrollLock.disableScrolling();
+    };
+
+    mobileMenu.addEventListener('click', (event) => {
+      const {target} = event;
+
+      if (target.classList.contains('mobile-menu__navigation')) {
+        close();
+      }
+
+      if (target.classList.contains('mobile-menu__toggle')) {
+        if (mobileMenu.classList.contains('is-close')) {
+          open();
+        } else {
+          close();
+        }
+      }
+
+      if (target.closest('.navigation__link')) {
+        event.preventDefault();
+
+        close();
+        createSmoothEffect(target);
+      }
+    });
+
+    mobileMenuNavigation.addEventListener('scroll', () => {
+      if (mobileMenu.classList.contains('is-open')) {
+        mobileMenuLogo.style.top = `${DefaultPosition.LOGO_OPEN - mobileMenuNavigation.scrollTop}px`;
+        mobileMenuToggle.style.top = `${DefaultPosition.TOGGLE_OPEN - mobileMenuNavigation.scrollTop}px`;
       }
     });
   }
--- source/js/utils/scroll-smooth.js
+++ source/js/utils/scroll-smooth.js
@@ -1,21 +1,25 @@
-const smoothLinks = document.querySelectorAll('a[data-smooth-link]');
+const smoothLinks = document.querySelectorAll('[data-smooth-link]');
+
+export const createSmoothEffect = (element) => {
+  const id = element.getAttribute('href');
+  const targetBlock = document.querySelector(id);
+
+  if (targetBlock) {
+    targetBlock.scrollIntoView({
+      behavior: 'smooth',
+      block: 'start',
+    });
+  }
+};
 
 export const createSmoothScroll = () => {
   if (smoothLinks.length) {
     [...smoothLinks].forEach((link) => {
-      const id = link.getAttribute('href');
-      const targetBlock = document.querySelector(id);
+      link.addEventListener('click', (event) => {
+        event.preventDefault();
 
-      if (targetBlock) {
-        link.addEventListener('click', (event) => {
-          event.preventDefault();
-
-          targetBlock.scrollIntoView({
-            behavior: 'smooth',
-            block: 'start',
-          });
-        });
-      }
+        createSmoothEffect(event.target);
+      });
     });
   }
 };
