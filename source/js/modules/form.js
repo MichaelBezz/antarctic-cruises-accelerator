f7edb8377b7555331a1f04d2fcadd42a0636c4d3
Michael Bezzaponnyy <michael.bezzaponnyy@gmail.com>
2022-11-18 10:29:27 +0300

    Фиксит валидацию формы

--- source/js/modules/form.js
+++ source/js/modules/form.js
@@ -4,34 +4,12 @@ const initializeValidation = (form) => {
   const phone = form.querySelector('[data-phone]');
 
   if (phone) {
-    const prefixNumber = (str) => {
-      if (str === '9') {
-        return '89';
-      }
-      return '8';
-    };
-
     phone.addEventListener('input', () => {
-      const value = phone.value.replace(/\D/g, '');
-      const numberLength = 11;
-
+      const value = phone.value.replace(/[а-яА-ЯёЁa-zA-Z]/, '');
       let result = '';
 
-      for (let i = 0; i < value.length && i < numberLength; i++) {
-        switch (i) {
-          case 0:
-            result += prefixNumber(value[i]);
-            continue;
-          default:
-            break;
-        }
+      for (let i = 0; i < value.length; i++) {
         result += value[i];
-      }
-
-      if (result.length < numberLength || result.length > numberLength) {
-        phone.setCustomValidity(`Телефон содержит ${result.length} из ${numberLength} цифр`);
-      } else {
-        phone.setCustomValidity('');
       }
 
       phone.value = result;
